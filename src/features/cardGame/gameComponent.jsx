a1c42896a2733e2e043efac25637678be0fb2dc3
Damian Wujec <wujecdamian12@gmail.com>
2026-05-10 21:57:19 +0200

    feat: add shuffleArray function (not functional - not operating on state)

--- src/features/cardGame/gameComponent.jsx
+++ src/features/cardGame/gameComponent.jsx
@@ -3,6 +3,34 @@ import Card from "./card.jsx";
 import Scoreboard from "./scoreboard.jsx";
 
 export default function GameComponent() {
+  const [cardsObjects, setCardsObjects] = [
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+    { name: "", src: "", hasBeenClicked: false },
+  ];
+
+  function shuffleArray() {
+    let i = Array.length,
+      random,
+      temp;
+    while (i > 1) {
+      random = Math.floor(Math.random() * (i + 1));
+      temp = cardsObjects[random];
+      cardsObjects[random] = cardsObjects[i];
+      cardsObjects[i] = temp;
+      i--;
+    }
+  }
+
   return (
     <>
       <Scoreboard></Scoreboard>
@@ -10,3 +38,4 @@ export default function GameComponent() {
     </>
   );
 }
+is;
