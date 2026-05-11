e30b6d3feeb9c8b7e624571f860a7e9c9d6a77fe
Damian Wujec <wujecdamian12@gmail.com>
2026-05-11 21:45:05 +0200

    refactor(scoreboard): update props to accept score and bestScore

--- src/features/cardGame/scoreboard.jsx
+++ src/features/cardGame/scoreboard.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 
-export default function Scoreboard() {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-
+export default function Scoreboard({ score, bestScore }) {
   return (
-    <section className="score_board">
+    <section className="scoreboard">
       <span>Score: {score}</span>
       <span>Best score: {bestScore}</span>
     </section>
